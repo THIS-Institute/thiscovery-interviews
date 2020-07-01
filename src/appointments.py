229038a847c11708db035618805751b70eaa8d6d
André Sartori <afs25@cam.ac.uk>
2020-07-01 09:27:26 +0100

    # 3412 comment: Added a core API client

--- src/appointments.py
+++ src/appointments.py
@@ -21,6 +21,8 @@ from http import HTTPStatus
 
 import common.utilities as utils
 from common.acuity_utilities import AcuityClient
+from common.core_api_utilities import CoreApiClient
+from common.dynamodb_utilities import Dynamodb
 
 
 class AcuityAppointmentEvent:
@@ -28,7 +30,11 @@ class AcuityAppointmentEvent:
 
     def __init__(self, acuity_event, logger, correlation_id=None):
         self.logger = logger
-        self.acuity_client = AcuityClient(correlation_id=correlation_id)
+        self.correlation_id = correlation_id
+        self.acuity_client = AcuityClient(correlation_id=self.correlation_id)
+        self.ddb_client = Dynamodb()
+        self.core_api_client = CoreApiClient(correlation_id=self.correlation_id)
+
         event_pattern = re.compile(
             r"action=appointment\.(?P<action>scheduled|rescheduled|canceled|changed)"
             r"&id=(?P<appointment_id>\d+)"
@@ -44,25 +50,13 @@ class AcuityAppointmentEvent:
             self.logger.error('event_pattern does not match acuity_event', extra={'acuity_event': acuity_event})
             raise
 
-    @staticmethod
-    def get_user_id_from_core_api(email):
-        # todo: this same code is used in stack s3-to-sdhs; create a core api client in utilities to avoid duplication
-        env_name = utils.get_environment_name()
-        if env_name == 'prod':
-            core_api_url = 'https://api.thiscovery.org/'
-        else:
-            core_api_url = f'https://{env_name}-api.thiscovery.org/'
-        result = utils.aws_get('v1/user', core_api_url, params={'email': email})
-
-        assert result['statusCode'] == HTTPStatus.OK, f'Call to core API returned error: {result}'
-        return json.loads(result['body'])['id']
-
     def get_appointment_details(self):
         r = self.acuity_client.get_appointment_by_id(self.appointment_id)
         return r['email'], r['appointmentTypeID']
 
-    def get_project_task_id(self):
-
+    def get_project_task_id_and_status(self):
+        item = self.ddb_client.get_item(self.appointment_type_table, self.type_id, correlation_id=self.correlation_id)
+        return item['project_task_id'], item['status']
 
     def main(self):
         self.logger.info('Parsed Acuity event', extra={'action': self.action, 'appointment_id': self.appointment_id, 'type_id': self.type_id})
@@ -70,11 +64,22 @@ class AcuityAppointmentEvent:
         assert str(appointment_type_id) == self.type_id, f'Unexpected appointment type id ({appointment_type_id}) in get_appointment_by_id response. ' \
                                                          f'Expected: {self.type_id}.'
         try:
-            user_id = self.get_user_id_from_core_api(email)
+            user_id = self.core_api_client.get_user_id_by_email(email)
         except Exception as err:
             self.logger.error(f'Failed to retrieve user_id for {email}', extra={'exception': repr(err)})
             raise err
 
+        try:
+            project_task_id, appointment_type_status = self.get_project_task_id_and_status()
+        except Exception as err:
+            self.logger.error(f'Failed to retrieve project_task_id for appointment_type_id {self.type_id}', extra={'exception': repr(err)})
+            raise err
+
+        if appointment_type_status in ['active']:
+            user_task_id = self.core_api_client.get_user_task_id_for_project(user_id, project_task_id)
+            result = self.core_api_client.set_user_task_completed(user_task_id)
+            self.logger.info(f'Updated user task {user_task_id} status to complete')
+            return result
 
 
 @utils.lambda_wrapper
---
+++ src/common/core_api_utilities.py
@@ -0,0 +1,50 @@
+#
+#   Thiscovery API - THIS Institute’s citizen science platform
+#   Copyright (C) 2019 THIS Institute
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   A copy of the GNU Affero General Public License is available in the
+#   docs folder of this project.  It is also available www.gnu.org/licenses/
+#
+import json
+from http import HTTPStatus
+
+import common.utilities as utils
+
+
+class CoreApiClient:
+
+    def __init__(self, correlation_id=None):
+        self.correlation_id = correlation_id
+        env_name = utils.get_environment_name()
+        if env_name == 'prod':
+            self.base_url = 'https://api.thiscovery.org/'
+        else:
+            self.base_url = f'https://{env_name}-api.thiscovery.org/'
+
+    def get_user_id_by_email(self, email):
+        # todo: similar code is used in stack s3-to-sdhs; adapt that stack to use this client
+        result = utils.aws_get('v1/user', self.base_url, params={'email': email})
+        assert result['statusCode'] == HTTPStatus.OK, f'Call to core API returned error: {result}'
+        return json.loads(result['body'])['id']
+
+    def get_user_task_id_for_project(self, user_id, project_task_id):
+        result = utils.aws_get('v1/usertask', self.base_url, params={'user_id': user_id})
+        assert result['statusCode'] == HTTPStatus.OK, f'Call to core API returned error: {result}'
+        for user_task in result:
+            if user_task['project_task_id'] == project_task_id:
+                return user_task['user_task_id']
+
+    def set_user_task_completed(self, user_task_id):
+        result = utils.aws_request('PUT', '/v1/user-task-completed', self.base_url, params={'user_task_id': user_task_id})
+        assert result['statusCode'] == HTTPStatus.NO_CONTENT, f'Call to core API returned error: {result}'
+        return result
